2914033a02d3bc3328a644dc4c59e9dbdd67e8eb
gustavo <gcostacoelho2003@gmail.com>
2023-05-25 19:53:21 -0300

    swagger-document

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from '../services/app.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('hello')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Elara server')
+    .setVersion('1.0')
+    .addTag('hello')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
