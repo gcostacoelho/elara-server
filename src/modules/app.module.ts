84115d90cec1a97a2826e6f6acd15acb0470207c
gustavo <gcostacoelho2003@gmail.com>
2023-07-19 17:56:46 -0300

    update-modules

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,9 +2,10 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthMiddleware } from 'src/middleware/auth/auth.middleware';
 import { UserModule } from './user.module';
 import { UserController } from 'src/controllers/user.controller';
+import { AuthModule } from './auth.module';
 
 @Module({
-    imports: [UserModule]
+    imports: [UserModule, AuthModule]
 })
 export class AppModule {
     configure(consumer: MiddlewareConsumer) {
