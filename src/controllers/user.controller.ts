fa8b43148eb9987a7cc6d3f179702200195a5f80
gustavo <gcostacoelho2003@gmail.com>
2023-07-21 17:41:52 -0300

    implements-service-create

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { UserService } from '../services/user.service';
+import { Response, Request } from 'express';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { UserDtoPass } from 'src/Models/User/Dtos/UserDtoPass';
 
 @ApiTags('User')
 @ApiBearerAuth()
-@Controller()
+@Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService) { }
 
+    @Post('add')
+    async createNewUser(@Body() user: UserDtoPass, @Res() resp: Response) {
+        const data = await this.userService.Create(user);
+
+        return resp.status(data.statusCode).json(data.body);
+    }
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { error } from 'console';
+import { UserDtoPass } from 'src/Models/User/Dtos/UserDtoPass';
+import { User } from 'src/Models/User/User';
+import { PrismaConfig } from 'src/database/prismaConfig';
+import { Crud } from 'src/interfaces/crud.interface';
+import { HttpResponse, badRequest, created, serviceError, success } from 'src/types/http';
 
 @Injectable()
-export class UserService { }
+export class UserService implements Crud {
+    constructor(private readonly prisma: PrismaConfig) { }
+
+    async Create(data: UserDtoPass): Promise<HttpResponse> {
+        try {
+            const user = new User(data.nome, data.email, data.dataNascimento, data.senha);
+
+            data.senha = await user.encriptyPassword();
+    
+            const newUser = await this.prisma.usuario.create({
+                data,
+            });
+    
+            return created(data);
+        } catch (error) {
+            return serviceError(error);
+        }
+    }
+
+    Read(id: string = ""): Promise<HttpResponse> {
+        throw new Error('Method not implemented.');
+    }
+
+    Update(data: Object, id: string): Promise<HttpResponse> {
+        throw new Error('Method not implemented.');
+    }
+
+    Delete(id: string): Promise<HttpResponse> {
+        throw new Error('Method not implemented.');
+    }
+}
