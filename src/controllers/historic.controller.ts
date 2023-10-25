0b270bfc2540d857d69c0c7c79799aff11f246e9
gustavo <gcostacoelho2003@gmail.com>
2023-10-25 20:29:10 -0300

    feat: controller for postNewHistoric

--- src/controllers/historic.controller.ts
+++ src/controllers/historic.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Delete, Get, Param, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Res } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Response } from "express";
 import { HistoricService } from '../services/historic.service';
+import { HistoricDto } from 'src/Models/History/Dtos/HistoricDto';
 
 @ApiTags('Historic')
 @ApiBearerAuth()
@@ -10,16 +11,23 @@ export class HistoricController {
     constructor(private readonly historicService: HistoricService) { }
 
     @Get(':email')
-    async getHistoric(@Param('email') email: string, @Res() resp: Response){
+    async getHistoric(@Param('email') email: string, @Res() resp: Response) {
         const data = await this.historicService.readHistoric(email);
 
         return resp.status(data.statusCode).json(data.body);
     }
 
     @Delete(':email')
-    async deleteHistoric(@Param('email') email: string, @Res() resp: Response){
+    async deleteHistoric(@Param('email') email: string, @Res() resp: Response) {
         const data = await this.historicService.deleteHistoric(email);
 
         return resp.status(data.statusCode).json(data.body);
+    }
+
+    @Post(':email')
+    async postNewHistoric(@Param('email') email: string, @Body() body: HistoricDto, @Res() res: Response) {
+        const data = await this.historicService.createHistoric(body, email);
+
+        return res.status(data.statusCode).json(data.body);
     }
 }
